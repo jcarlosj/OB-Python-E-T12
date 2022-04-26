c139ad753028a3f28a491e9bcb8c9f06db0a42c0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-04-26 14:17:38 -0500

    [+] App (Movies): Define Models (Director, Movie)

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+# Director Model
+class Director( models.Model ) :
+    first_name = models.CharField( max_length = 100 )
+    last_name = models.CharField( max_length = 100 )
+    date_of_birth = models.DateField( null = True, blank = True )
+    date_of_death = models.DateField( 'Died', null = True, blank = True )
+
+    def get_absolute_url( self ) :
+        return reverse( 'director-detail', args=[ str( self.id ) ] )
+
+    def __str__( self ) :
+        return f'{ self.first_name } { self.last_name } ({ self.date_of_birth })'
+
+# Movie Model
+class Movie( models.Model ) :
+    title = models.CharField( max_length = 32 )
+    director = models.ForeignKey( 
+        Director,                       # Relacion: Con el Modelo Director (Haciendo uso de una llave foranea)
+        on_delete = models.SET_NULL,    # Permite eliminar el director del registro
+        null = True                     # Permite que este campo sea nulo (Mantiene la integridad relacional)
+    )
+    synopsis = models.TextField( max_length = 250, help_text = 'Breve sinópsis de la película')
+    release_date = models.DateField( null = True, blank = True )
+
+    def get_absolute_url( self ) :
+        return reverse( 'movie-detail', args=[ str( self.id ) ] )
+
+    def __str__( self ) :
+        return f'{ self.title } ({ self.director })'
